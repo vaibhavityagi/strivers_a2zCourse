b8e2f687b35fe57b636f4a0888d8aa82cb8e5483
vaibhavi tyagi <vaibhavityagi5@gmail.com>
2023-05-05 18:47:31 +0530

    optimal approach of longest subarray with sum k (only positives)

--- arrays_strings/easy.cpp
+++ arrays_strings/easy.cpp
@@ -515,8 +515,31 @@ void longestSubArrK(int a[], int k, int n)
     cout << "Maximum lenght of the subarray having sum " << k << ": " << maxLen;
 }
 
+// TC = O(2N)
 void longestSubArrKOpt(int a[], int k, int n)
 {
+    int maxLen = 0;
+    int sum = 0;
+    int right = 0, left = 0;
+    while (right < n)
+    {
+        sum += a[right];
+
+        while (left <= right && sum > k)
+        {
+            sum -= a[left];
+            left++;
+        }
+
+        if (sum == k)
+        {
+            maxLen = max(maxLen, right - left + 1);
+        }
+
+        right++;
+    }
+
+    cout << maxLen;
 }
 
 int main()
@@ -547,8 +570,8 @@ int main()
     // for (auto it : ans)
     //     cout << it << " ";
 
-    int array[] = {2, 3, 5, 1, 9};
+    int array[] = {1, 2, 3, 1, 1, 1, 1, 3, 3};
     int n = sizeof(array) / sizeof(array[0]);
-    longestSubArrKOpt(array, 10, 5);
+    longestSubArrKOpt(array, 6, 5);
     return 0;
 }
